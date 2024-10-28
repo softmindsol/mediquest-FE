9fe21151ec5cf7180fb5d57e67e065419c79113d
Taimoor Hussain <taimoorhussain985@gmail.com>
2024-10-28 15:59:23 +0500

    ✨ NEW FEATURE: Landing page

--- src/pages/landingpage/LandingPage.jsx
+++ src/pages/landingpage/LandingPage.jsx
@@ -15,7 +15,7 @@ const LandingPage = () => {
     <>
               <Header />
 
-      <div className="flex justify-end gap-5 mb-8 mt-12 px-18">
+      <div className="flex justify-end gap-5 mt-12 mb-8 px-18">
         <Link to="/log-in">
           <button className="bg-[#3A57E8] font-semibold text-title-p text-white py-[10px] px-8 rounded-[6px]">
            Log in
